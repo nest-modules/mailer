063e910dca5c1d8219cb4dbdb08a8ab71b9d4718
Cristiam Díaz <c.diaz@udla.edu.co>
2019-04-09 13:29:35 -0500

    fix sendmailoptions interface

--- lib/interfaces/send-mail-options.interface.ts
+++ lib/interfaces/send-mail-options.interface.ts
@@ -1,6 +1,11 @@
 import { SendMailOptions } from 'nodemailer';
 
 export interface ISendMailOptions extends SendMailOptions {
+  to?: string;
+  from?: string;
+  subject?: string;
+  text?: string;
+  html?: string;
   template?: string,
   context?: { [name: string]: any; }
 }
