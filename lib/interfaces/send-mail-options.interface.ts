bfef2730651ea6eedf630c195d6e8c9c64775674
Nelson Bwogora <nelsonbwogora@gmail.com>
2019-12-04 19:49:30 +0300

    update SendMailOptions to include file attachments (#51)

--- lib/interfaces/send-mail-options.interface.ts
+++ lib/interfaces/send-mail-options.interface.ts
@@ -2,10 +2,22 @@ import { SendMailOptions } from 'nodemailer';
 
 export interface ISendMailOptions extends SendMailOptions {
   to?: string;
-  from?: string;
-  subject?: string;
-  text?: string;
-  html?: string;
-  template?: string,
-  context?: { [name: string]: any; }
+    cc?:string[];
+    from?: string;
+    subject?: string;
+    text?: string;
+    html?: string;
+    template?: string;
+    context?: {
+        [name: string]: any;
+    };
+    attachments?: {
+        filename: string,
+        contents?: any,
+        path?: string,
+        contentType?: string
+        cid: string
+    }[]
 }
+
+ 
