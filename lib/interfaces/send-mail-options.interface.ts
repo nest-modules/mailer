02e5950e01ac11ff6ee00a7fec0b9dd2b41b3da3
juandav <e.juandav@gmail.com>
2020-03-19 16:16:50 -0500

    fix(): sendMail options errors #85

--- lib/interfaces/send-mail-options.interface.ts
+++ lib/interfaces/send-mail-options.interface.ts
@@ -1,6 +1,31 @@
 import { SendMailOptions } from 'nodemailer';
 
+export type TextEncoding = 'quoted-printable' | 'base64';
+export type Headers =
+  | { [key: string]: string | string[] | { prepared: boolean; value: string } }
+  | Array<{ key: string; value: string }>;
+
+export interface Address {
+  name: string;
+  address: string;
+}
+
 export interface ISendMailOptions extends SendMailOptions {
+  to?: string | Address | Array<string | Address>;
+  cc?: string | Address | Array<string | Address>;
+  replyTo?: string | Address;
+  inReplyTo?: string | Address;
+  from?: string | Address;
+  subject?: string;
+  text?: string | Buffer;
+  html?: string | Buffer;
+  sender?: string | Address;
+  raw?: string | Buffer;
+  textEncoding?: TextEncoding;
+  references?: string | string[];
+  encoding?: string;
+  date?: Date | string;
+  headers?: Headers;
   context?: {
     [name: string]: any;
   };
