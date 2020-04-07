3315fb3eda14b9e5cd8a8541beacf76e49d22915
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-04-07 12:09:50 -0600

    sample(02-custom-template-adapter): Configure SMTP connection
    Use MailDev as SMTP server.
    Run services with Docker Composer.

--- sample/02-custom-template-adapter/src/app.module.ts
+++ sample/02-custom-template-adapter/src/app.module.ts
@@ -1,9 +1,23 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST || 'localhost',
+        port: parseInt(process.env.SMTP_PORT, 10) || 1025,
+        secure: process.env.SMTP_SECURE === 'true',
+        ignoreTLS: process.env.SMTP_SECURE !== 'false',
+        auth: {
+          user: process.env.SMTP_AUTH_USER || 'username',
+          pass: process.env.SMTP_AUTH_PASS || 'password',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
