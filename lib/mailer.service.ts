3d4252745ca995d5fa27a55068727549847b7801
frezyn <00001097715413SP@al.educacao.sp.gov.br>
2024-03-25 17:39:29 -0300

    Atualizar log de depuração para registro de transporte

--- lib/mailer.service.ts
+++ lib/mailer.service.ts
@@ -106,7 +106,7 @@ export class MailerService {
   private verifyTransporter(transporter: Transporter, name?: string): void {
     const transporterName = name ? ` '${name}'` : '';
     transporter.verify()
-      .then(() => this.mailerLogger.debug(`Transporter${transporterName} is ready`))
+      .then(() => this.mailerLogger.log(`Transporter${transporterName} is ready`))
       .catch((error) => this.mailerLogger.error(`Error occurred while verifying the transporter${transporterName}: ${error.message}`));
   }
 
